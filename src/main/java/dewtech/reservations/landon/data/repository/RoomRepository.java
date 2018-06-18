8ed138633601b691a02d05c1d64c68ec93de17c6
Wanderson Leandro de Oliveira <wleandro.oliveira@gmail.com>
2018-06-18 19:57:17 -0300

    Create Class Room, Interface RooRepositiry and RoomController

---
+++ src/main/java/dewtech/reservations/landon/data/repository/RoomRepository.java
@@ -0,0 +1,11 @@
+package dewtech.reservations.landon.data.repository;
+
+import dewtech.reservations.landon.data.entity.Room;
+import org.springframework.data.repository.CrudRepository;
+import org.springframework.stereotype.Repository;
+
+@Repository
+public interface RoomRepository extends CrudRepository<Room, Long>{
+    Room findByNumber(String number);
+
+}
